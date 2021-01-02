7f1cbf2ec58f096bd8db9080a969ba504bc80bc5
Elliot-JG <ejg27@student.le.ac.uk>
2021-01-02 10:34:39 +0000

    Normalised output

--- Plotting_histograms.R
+++ Plotting_histograms.R
@@ -7,45 +7,47 @@ library(magrittr)
 
 # FIX THESE PATHS!! DATA NOW IN DATA FOLDER 
 
-clouds <- readr::read_csv("Clouds.csv")
-land <- readr::read_csv("Land.csv")
-smog <- readr::read_csv("Smog.csv")
+clouds <- readr::read_csv("Data/Clouds.csv")
+land <- readr::read_csv("Data/Land.csv")
+smog <- readr::read_csv("Data/Smog.csv")
 
 # Frequency of reflectance values in B4 
 
 cloud_hist <- ggplot(clouds, aes(
     x = Bin_center_value, 
-    y = Bin_counts))+
+    y = Bin_counts/1214))+
   geom_bar(
     stat='identity',
     fill = "red")+
   scale_x_binned(
     n.breaks = 20)+
   xlab(expression(R[rs]~at~665~nm))+
-  ylab("Frequency")
+  ylab("Frequency")+
+  theme(text = element_text(size=27))
 
 land_hist <- ggplot(land, aes(
     x = Bin_center_value, 
-    y = Bin_counts))+
+    y = Bin_counts/6657))+
   geom_bar(
     stat='identity',
     fill = "green3")+
   scale_x_binned(
     n.breaks = 20)+
   xlab(expression(R[rs]~at~665~nm))+
-  ylab("Frequency")
+  ylab("Frequency")+
+  theme(text = element_text(size=27))
 
 smog_hist <- ggplot(smog, aes(
   x = Bin_center_value, 
-  y = Bin_counts))+
+  y = Bin_counts/4573))+
   geom_bar(
     stat='identity',
     fill = "blue")+
   scale_x_binned(
     n.breaks = 20)+
   xlab(expression(R[rs]~at~665~nm))+
-  ylab("Frequency")
+  ylab("Frequency")+
+  theme(text = element_text(size=27))
 
-plot(smog_hist)
 
 grid.arrange(cloud_hist, smog_hist, land_hist)
